45bf34f141637c549daa646e72857e7094d2748a
Jack Rose <user@jackrose.co.nz>
2024-03-15 10:58:07 +1100

    move upgraded flake to folder, re-add un-upgraded flake

---
+++ flake-1/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.bashInteractive ];
+        };
+      });
+}
